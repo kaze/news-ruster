3519574d8a8f66a268dae4fa3a787a9dc091e420
zsolt kormany <zsoltkormany@gmail.com>
2023-09-04 01:26:13 +0200

    add `health_check` endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use newsruster::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let message = &format!("Healt check url: http://127.0.0.1:{}/health_check", &port);
+
+    println!("{message}");
+
+    let _server = run(listener).expect("Failed to bind address").await;
+
+    Ok(())
 }
